9f46ba66531a4528d011849d1fce67030597c95d
SharonBH <sharon.benhaim@gmail.com>
2025-07-01 10:00:27 +0300

    SeedAsync fix

--- src/api/modules/Catalog/Catalog.Infrastructure/Persistence/CatalogDbInitializer.cs
+++ src/api/modules/Catalog/Catalog.Infrastructure/Persistence/CatalogDbInitializer.cs
@@ -34,84 +34,14 @@ internal sealed class CatalogDbInitializer(
         {
             var agency = Agency.Create(name, email, Telephone, Address, Description, LogoURL, PrimaryColor, additionalInfo);
             await context.Agencies.AddAsync(agency, cancellationToken);
-        }
 
-        //const string RegionName = "גוש דן";
-        //const string RegionDescription = "גוש דן";
-        //const string Region2Name = "צפון";
-        //const string Region2Description = "צפון";
-        //const string Region3Name = "מרכז";
-        //const string Region3Description = "מרכז";
-        //const string Region4Name = "דרום";
-        //const string Region4Description = "דרום";
-
-        //if (await context.Regions.FirstOrDefaultAsync(t => t.Name == RegionName, cancellationToken).ConfigureAwait(false) is null)
-        //{
-        //    var region = Region.Create(RegionName, RegionDescription);
-        //    var region2 = Region.Create(Region2Name, Region2Description);
-        //    var region3 = Region.Create(Region3Name, Region3Description);
-        //    var region4 = Region.Create(Region4Name, Region4Description);
-
-        //    await context.Regions.AddAsync(region, cancellationToken);
-        //    await context.Regions.AddAsync(region2, cancellationToken);
-        //    await context.Regions.AddAsync(region3, cancellationToken);
-        //    await context.Regions.AddAsync(region4, cancellationToken);
-
-        //const string CityName = "תל אביב";
-
-        //    var city1 = City.Create(CityName, CityName, region.Id);
-        //    var city2 = City.Create("חיפה", "חיפה", region2.Id);
-        //    var city3 = City.Create("ראשון לציון", "ראשון לציון", region3.Id);
-        //    var city4 = City.Create("באר שבע", "באר שבע", region4.Id);
-
-        //    await context.Cities.AddAsync(city1, cancellationToken);
-        //    await context.Cities.AddAsync(city2, cancellationToken);
-        //    await context.Cities.AddAsync(city3, cancellationToken);
-        //    await context.Cities.AddAsync(city4, cancellationToken);
-
-        //    const string NeighborhoodName = "צהלה";
-        //    const string NeighborhoodDescription = "צהלה";
-        //    const string SphereURL = "16.jpg";
-        //    const string IconURL = "16.jpg";
-        //    const double Score = 4.5;
-
-        //    var neighborhood1 = Neighborhood.Create(NeighborhoodName, NeighborhoodDescription, city1.Id, SphereURL, IconURL, Score);
-        //    var neighborhood2 = Neighborhood.Create("רמת אביב", "רמת אביב", city1.Id, SphereURL, IconURL, Score);
-        //    var neighborhood3 = Neighborhood.Create("כרמל", "כרמל", city2.Id, SphereURL, IconURL, Score);
-        //    var neighborhood4 = Neighborhood.Create("עתלית", "עתלית", city2.Id, SphereURL, IconURL, Score);
-        //    var neighborhood5 = Neighborhood.Create("על הים", "על הים", city3.Id, SphereURL, IconURL, Score);
-        //    var neighborhood6 = Neighborhood.Create("אוניברסיטה", "אוניברסיטה", city4.Id, SphereURL, IconURL, Score);
-
-        //    await context.Neighborhoods.AddAsync(neighborhood1, cancellationToken);
-        //    await context.Neighborhoods.AddAsync(neighborhood2, cancellationToken);
-        //    await context.Neighborhoods.AddAsync(neighborhood3, cancellationToken);
-        //    await context.Neighborhoods.AddAsync(neighborhood4, cancellationToken);
-        //    await context.Neighborhoods.AddAsync(neighborhood5, cancellationToken);
-        //    await context.Neighborhoods.AddAsync(neighborhood6, cancellationToken);
-
-        //}
-
-        //if (await context.PropertyTypes.FirstOrDefaultAsync(cancellationToken).ConfigureAwait(false) is null)
-        //{
-        //    var propertyType1 = PropertyType.Create("דירה", "דירה");
-        //    var propertyType2 = PropertyType.Create("בית פרטי", "בית פרטי");
-        //    var propertyType3 = PropertyType.Create("דירת גן", "דירת גן");
-        //    var propertyType4 = PropertyType.Create("פנטהאוז", "פנטהאוז");
-        //    var propertyType5 = PropertyType.Create("מיני פנטהאוז", "מיני פנטהאוז");
-        //    var propertyType6 = PropertyType.Create("דירת גג", "דירת גג");
-        //    var propertyType7 = PropertyType.Create("קוטג", "קוטג");
-
-        //    await context.PropertyTypes.AddAsync(propertyType1, cancellationToken);
-        //    await context.PropertyTypes.AddAsync(propertyType2, cancellationToken);
-        //    await context.PropertyTypes.AddAsync(propertyType3, cancellationToken);
-        //    await context.PropertyTypes.AddAsync(propertyType4, cancellationToken);
-        //    await context.PropertyTypes.AddAsync(propertyType5, cancellationToken);
-        //    await context.PropertyTypes.AddAsync(propertyType6, cancellationToken);
-        //    await context.PropertyTypes.AddAsync(propertyType7, cancellationToken);
-        //}
 
             await context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
             logger.LogInformation("[{Tenant}] seeding default catalog data", context.TenantInfo!.Identifier);
-        
+        }
+        else
+        {
+            logger.LogInformation("[{Tenant}] catalog data already seeded", context.TenantInfo!.Identifier);
+        }
     }
 }
